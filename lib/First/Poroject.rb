4a375331668518246475759d837ec03d8a89c60a
rezanrezano <captenrez@gmail.com>
2017-01-31 12:12:14 +0000

    first-step

---
+++ lib/First/Poroject.rb
@@ -0,0 +1,7 @@
+require "First/Poroject/version"
+
+module First
+  module Poroject
+    # Your code goes here...
+  end
+end
---
+++ lib/First/Poroject/version.rb
@@ -0,0 +1,5 @@
+module First
+  module Poroject
+    VERSION = "0.1.0"
+  end
+end
